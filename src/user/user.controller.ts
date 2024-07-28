7becec471d3713006dd44215a4ae29c1c93cb937
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-07-28 20:00:17 -0300

    feat: creating simple crud

---
+++ src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body: CreateUserDto) {
+    return body;
+  }
+
+  @Get()
+  async readAll() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() params) {
+    return { user: { params } };
+  }
+
+  @Patch(':id')
+  async update(@Body() body: Partial<CreateUserDto>, @Param() params) {
+    return { user: { body, params } };
+  }
+}
