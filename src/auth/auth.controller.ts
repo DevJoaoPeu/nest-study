3173280f98cfbf11f158110644e6cd50251ea674
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-08-14 22:17:08 -0300

    feat: acessando metodos do serviço no controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,18 @@ import { AuthRegisterDto } from './dto/auth-register.dto';
 import { AuthForgetDto } from './dto/auth-forget.dto';
 import { AuthResetDto } from './dto/auth-reset.dto';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authServive: AuthService,
+  ) {}
   @Post('login')
-  async login(@Body() body: AuthLoginDto) {}
+  async login(@Body() { email, password }: AuthLoginDto) {
+    return this.authServive.login(email, password);
+  }
 
   @Post('register')
   async register(@Body() body: AuthRegisterDto) {
@@ -17,8 +23,12 @@ export class AuthController {
   }
 
   @Post('forget')
-  async forget(@Body() body: AuthForgetDto) {}
+  async forget(@Body() { email }: AuthForgetDto) {
+    return this.authServive.forget(email);
+  }
 
   @Post('reset')
-  async reset(@Body() body: AuthResetDto) {}
+  async reset(@Body() { password, token }: AuthResetDto) {
+    return this.authServive.reset(password, token);
+  }
 }
